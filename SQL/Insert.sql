e02684fb35925d132931deb0058224b11621a036
Ivan Labrador <ivanrlg@gmail.com>
2022-09-17 12:50:50 -0400

    Insert SQL Script

---
+++ SQL/Insert.sql
@@ -0,0 +1,7 @@
+INSERT 
+INTO [DB_NAV80].[dbo].[Records Links Temp$9c054801-914b-4aa7-bb6b-888888888888]
+([Link ID],[Record ID],[URL1],[Description],[Type],[Note],[Created],[User ID],[Company],[Notify],[To User ID])
+
+SELECT [Link ID],[Record ID],[URL1],[Description],[Type],[Note],[Created],[User ID],[Company],[Notify],[To User ID]  
+FROM  
+[DB_NAV80].[dbo].[Record Link];
